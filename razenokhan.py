24117eab291f3884bd2b2a71513390a367b320ec
RazzzyK <Razenok@yahoo.com>
2024-10-11 14:38:42 -0400

    Added file output

--- razenokhan.py
+++ razenokhan.py
@@ -3,5 +3,8 @@
 def main():
     print("Hello, this is Razeno's Python application!")
 
+    with open('output.txt', 'w') as f:
+        f.write("Hello, this is Razeno's Python application!\n")
+
 if __name__ == "__main__":
     main()
